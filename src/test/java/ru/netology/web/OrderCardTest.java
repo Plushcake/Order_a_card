8e1296a35f8e23c6a4bf95f5f912afe37e745d03
Plushcake <Nikkolo.Makeawelli@gmail.com>
2022-07-29 19:40:17 +0300

    Modified OrderCardTest. Enable headless

--- src/test/java/ru/netology/web/OrderCardTest.java
+++ src/test/java/ru/netology/web/OrderCardTest.java
@@ -10,8 +10,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static io.github.bonigarcia.wdm.WebDriverManager.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -25,13 +28,19 @@ public class OrderCardTest {
         System.setProperty("webdriver.chrome.driver", "driver/windows/chromedriver.exe");
 
         chromedriver().setup();
-
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
         //Запускается перед каждым тестовым методом.
     void setup() {
         driver = new ChromeDriver();
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
 
@@ -47,8 +56,10 @@ public class OrderCardTest {
         List<WebElement> elements = driver.findElements(By.className("input__control"));
         elements.get(0).sendKeys("Александр Пушкин");
         elements.get(1).sendKeys("+79995557744");
+
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.tagName("button")).click();//Продолжить
+
         String text = driver.findElement(By.className("paragraph")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
